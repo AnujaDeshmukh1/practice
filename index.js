652d35338bbfcae3de9d163c1adcf4cb708b8a21
AnujaDeshmukh1 <anuja.deshmukh456@gmail.com>
2021-08-29 20:26:32 +0530

    sample

---
+++ index.js
@@ -0,0 +1,15 @@
+var a = 10;
+let b = 300;
+const c = 400;
+function x() {
+    var a = 20;
+    let b = 30; 
+    const c = 40;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
